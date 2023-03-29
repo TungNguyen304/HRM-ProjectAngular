f8eba368d24cb2669cd27266a99d26bf396c079f
TungNguyen304 <nguyenvantung2001dn@gmail.com>
2023-03-29 16:51:05 +0700

    change get api member to revice data from parent component

--- src/app/home/employee/components/member-table/member-table.component.ts
+++ src/app/home/employee/components/member-table/member-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MemberService } from 'src/app/core/services/http/member.service';
 
 @Component({
@@ -7,16 +7,13 @@ import { MemberService } from 'src/app/core/services/http/member.service';
   styleUrls: ['./member-table.component.scss']
 })
 export class MemberTableComponent {
-  public memberList:any;
   constructor(private memberService:MemberService) {}
-
+  @Input() memberList:any[]
   onPageChange(event:any) {
     console.log(event);
   }
 
   ngOnInit() {
-    this.memberService.getMember().subscribe((data: any) => {
-      this.memberList = data
-    })
+    console.log(this.memberList);
   }
 }
