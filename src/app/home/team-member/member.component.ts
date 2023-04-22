adb63a434d505fc5c8135ae45fceb5c031906755
TungNguyen304 <nguyenvantung2001dn@gmail.com>
2023-04-22 13:59:46 +0700

    fix UI team member

--- src/app/home/team-member/member.component.ts
+++ src/app/home/team-member/member.component.ts
@@ -16,59 +16,18 @@ import { IMember } from './team-member.component';
           <p class="birthday">{{ memberInfo.birth_date }}</p>
           <p class="phone">{{ memberInfo.mobile }}</p>
         </div>
+        <div class="action">
+          <button
+            pButton
+            pRipple
+            label="Chi tiết"
+            class="p-button-sm p-button-warning"
+          ></button>
+        </div>
       </div>
     </div>
   `,
-  styles: [
-    `
-      .team_item {
-        box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
-        cursor: pointer;
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-        background-color: #fff;
-        border-radius: 5px;
-        overflow: hidden;
-        .avt {
-          flex: 1;
-          img {
-            width: 100%;
-            height: 250px;
-            object-fit: cover;
-            display: block;
-          }
-          i {
-            font-size: 120px;
-            color: var(--primary-color-main);
-          }
-        }
-        .info {
-          padding: 10px 20px 20px;
-          flex: 1;
-          display: flex;
-          background-color: var(--primary-color-main);
-          flex-direction: column;
-          justify-content: space-between;
-          color: #fff;
-        }
-        .name {
-          margin-bottom: 20px;
-          font-size: 20px;
-          font-weight: 600;
-          color: #fff;
-        }
-        .code,
-        .birthday {
-          margin-bottom: 20px;
-          display: -webkit-box;
-          -webkit-line-clamp: 1;
-          -webkit-box-orient: vertical;
-          overflow: hidden;
-        }
-      }
-    `,
-  ],
+  styleUrls: ['./member.component.scss'],
 })
 export class MemberComponent {
   @Input() memberInfo: IMember;
